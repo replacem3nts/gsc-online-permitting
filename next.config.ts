00f4ef17b69c098aac239ee85d56d661faf2648d
donny landis <donny@spinwheel.io>
2025-10-26 20:02:30 -0400

    trying to push and redeploy again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@vercel/og'],
+  serverExternalPackages: ['@vercel/og', '@prisma/client', 'prisma'],
 };
 
 export default nextConfig;
